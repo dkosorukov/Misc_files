ac5b5cac48f1903e2efabb8f7a87c1957e7aea7a
Dmitry Kosorukov <dmitrykosorukov@gmail.com>
2021-02-13 17:51:31 -0800

    Add find_paths function in routes.py

--- routes.py
+++ routes.py
@@ -1,6 +1,5 @@
 import csv
 import json
-
 import helper
 
 def read_airlines(filename='airlines.dat'):
@@ -19,7 +18,7 @@ def read_airports(filename='airports.dat'):
         reader = csv.reader(f)
         for line in reader:
             airport[line[4]] = line[1]
-    return airlines
+    return airport
 
 
 def read_routes(filename='routes.dat'):
@@ -34,14 +33,23 @@ def read_routes(filename='routes.dat'):
             routes[source].append(dest)
     return routes
 
-
-
-    return {}
-
-
 def find_paths(routes, source, dest, max_segments):
-    # Run a graph search algorithm to find paths from source to dest.
-    return {}
+    # Run the BFS search
+    frontier = {source}
+    seen = {source: {(source, )}}
+    for steps in range(max_segments):
+        next_frontier = set()
+        for airport in frontier:
+            for target in routes.get(airport, ()):
+                if target not in seen:
+                    next_frontier.add(target)
+                    seen[target] = set()
+                for path in seen[airport]:
+                    if len(path) != steps + 1:
+                        continue
+                    seen[target].add(path + (target, ))
+        frontier = next_frontier
+    return seen[dest]
 
 def rename_path(path, airports):
     return tuple(map(airports.get, path))
@@ -53,10 +61,16 @@ def main(source, dest, max_segments):
     routes = read_routes()
 
     paths = find_paths(routes, source, dest, max_segments)
-    output = {}  # Build a collection of output paths!
-    
-    # Don't forget to write the output to JSON!
+    output = {}  # Again, could use a collections.defaultdict(list) here.
+    for path in paths:
+        segments = len(path) - 1
+        if segments not in output:
+            output[segments] = []
+        output[segments].append(rename_path(path, airports))
 
+    with open(f"{source}->{dest} (max {max_segments}).json", 'w') as f:
+        json.dump(output, f, indent=2, sort_keys=True)
+        
 if __name__ == '__main__':
     parser = helper.build_parser()
     args = parser.parse_args()
