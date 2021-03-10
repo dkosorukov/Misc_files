6bc2999e0b00e90ecbdd286dc731b6081bb5539b
Dmitry Kosorukov <dmitrykosorukov@gmail.com>
2021-03-09 22:01:52 -0800

    Update weatherService.py
    remove appId

--- weatherService.py
+++ weatherService.py
@@ -1,7 +1,7 @@
 import requests
 
 class WeatherService:
-    appId = "0e6fa2e409066399c294ac1b125f0a32"
+    appId = ""
     baseUrl = "https://api.openweathermap.org/data/2.5/forecast" #?q={city name},{state code},{country code}&appid={API key}
     @classmethod
     def getForecast(cls, city, state, country):
